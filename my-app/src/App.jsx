a3862ea147b09e1617eff217fb6117a90740dd25
Jelena Stanojoski <stanojoskijelena997@gmail.com>
2025-12-17 19:27:56 +0100

    Implementiran prikaz postera i Like i Dislike tastera

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -3,31 +3,60 @@ import "./styles/main.scss"
 import Movie from "./movie"
 import Header from "./header";
 
-const movies = [
-  {
-    title: "Captain America - The First Avenger",
-    hall: 2,
-    price: 350
-  },
-  {
-    title: "The Papillon",
-    hall: 1,
-    price: 300
-  },
-  {
-    title: "The Lost City of Z",
-    hall: 5,
-    price: 350
-  }
-];
 
-export default () => (
-  <>
-    <Header />
+const App = () => {
+  const movies = [
     {
-      movies.map(movie => (
-        <Movie movieTitle={movie.title} hall={movie.hall} price={movie.price} />
-      ))
+      title: "Captain America - The First Avenger",
+      hall: 2,
+      price: 350,
+      poster: "https://m.media-amazon.com/images/I/51Xp+8qDCbL._AC_UF350,350_QL50_.jpg"
+    },
+    {
+      title: "The Papillon",
+      hall: 1,
+      price: 300,
+      poster: "https://m.media-amazon.com/images/M/MV5BMjIxMTMyOTE2NF5BMl5BanBnXkFtZTgwMDYyNzY1NTM@._V1_.jpg"
+    },
+    {
+      title: "The Lost City of Z",
+      hall: 5,
+      price: 350,
+      poster: "https://m.media-amazon.com/images/M/MV5BZmU2ODIyMWItMjU3Zi00ZmVhLWIyNDAtMWE5OWU2ZDExMGFiXkEyXkFqcGc@._V1_FMjpg_UX1000_.jpg"
+    },
+    {
+      title: "Klaus",
+      hall: 3,
+      poster: "https://m.media-amazon.com/images/I/7128yjOjl9L.jpg"
+    },
+    {
+      title: "Bullet Train",
+      poster: "https://m.media-amazon.com/images/I/71INz6LX8aL._AC_UF894,1000_QL80_.jpg"
     }
-  </>
-);
+  ];
+
+  const handleMessage = (movie, type) => {
+    if (type === "Like") {
+      alert(`Dodelili ste "Like" za film "${movie.title}"!`);
+    } else {
+      alert(`Dodelili ste "Dislike" za film "${movie.title}"!`);
+    }
+  }
+
+  return (
+    <>
+      <Header />
+      <div>
+        {
+          movies?.length > 0 ?
+            <div>
+              {movies.map(m => (<Movie movie={m} onMessage={handleMessage} />
+              ))}
+            </div> : (<p>Nema dostupnih filmova</p>)
+        }
+      </div>
+    </>
+  );
+};
+
+export default App;
--- my-app/src/movie.jsx
+++ my-app/src/movie.jsx
@@ -2,13 +2,24 @@ import movie from "./assets/movie.jpg"
 import React from "react";
 
 const Movie = (props) => {
+
+    const handleMesagge = (type) => {
+        props.onMessage(props.movie, type)
+    }
+
     return (
         < div className="container" >
             < div className="image" >
-                <img alt="movie" src={movie} />
+                <img alt="movie" src={props.movie.poster} />
             </div >
             <div className="movie">
-                {props.movieTitle}, {props.hall}, {props.price}
+                {props.movie.title},
+                {props.movie.hall ? " sala: " + props.movie.hall : " Film još uvek nije u ponudi"},
+                {props.movie.price ? " cena: " + props.movie.price + "din" : " 300din"}
+            </div>
+            <div className="buttons">
+                <button onClick={() => handleMesagge("Like")}>Like</button>
+                <button onClick={() => handleMesagge("Dislike")}>Dislike</button>
             </div>
         </div >
     )
